1f9d4e40a4e61454407ef8ae3fbee91e3543d3e2
AravindKarthikeyan <aravindkl1995@gmail.com>
2022-04-07 13:50:12 +0530

    User Counter update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
     <div>
       <div className="white f3">
-        {'Aravind, your current rank is....'}
+        {`${name}, your current entry count is....`}
       </div>
-      <div className="white f1">
-        {'#5'}
-      </div>
+      <div className="white f1">{entries}</div>
     </div>
   );
 };
